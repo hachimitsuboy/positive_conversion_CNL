650bdd019a66c887e0483ef17d1cc9c4c3726019
hachimitsuboy <39763423+hachimitsuboy@users.noreply.github.com>
2021-10-27 11:24:02 +0900

    選択された上位何件を表示させるしように変更

--- lib/get_value_page.dart
+++ lib/get_value_page.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:positive_conversion_cnl/data_class/score_data.dart';
-import 'package:positive_conversion_cnl/home_screen.dart';
+import 'package:positive_conversion_cnl/selected_one_page.dart';
+import 'package:positive_conversion_cnl/selected_other_page.dart';
 
 class GetValuePage extends StatefulWidget {
   final List<String> resultWords;
@@ -31,103 +32,29 @@ class _GetValuePageState extends State<GetValuePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("計算結果"),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text(
+          "計算結果",
+          style: TextStyle(color: Colors.white),
         ),
-        body: (scoreList.isEmpty)
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : Center(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 38,
-                    ),
-                    Text(
-                      "最もポジティブ値の高い用語",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w200,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      (maxScoreWord != "")
-                          ? "「$maxScoreWord」" //TODO
-                          : "0を超えるポジティブ値をもつ類語が見つかりませんでした",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 55,
-                    ),
-                    Center(
-                      child: Stack(
-                        children: [
-                          Center(
-                            child: Container(
-                              width: 200,
-                              height: 200,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.lightGreen,
-                              ),
-                            ),
-                          ),
-                          Center(
-                            child: Column(
-                              children: [
-                                SizedBox(
-                                  height: 32.0,
-                                ),
-                                Text(
-                                  "ポジティブ値",
-                                  style: TextStyle(
-                                      fontSize: 20.0, color: Colors.white),
-                                ),
-                                SizedBox(
-                                  height: 40,
-                                ),
-                                Text(
-                                  (maxScoreWord != "")
-                                      ? maxScore.toString()
-                                      : "0",
-                                  style: TextStyle(
-                                      fontSize: 32.0, color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 100,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          //TODO
-                          onPressed: () => toNextPage(context),
-                          child: Text("トップへ戻る"),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.lightGreen,
-                            onPrimary: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+        centerTitle: true,
+      ),
+      body: (scoreList.isEmpty)
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : (widget.selectedValue == 1)
+              ? Center(
+                  child: SelectedOnePage(
+                    scoreList: scoreList,
+                    selectedValue: widget.selectedValue,
+                  ),
+                )
+              : SelectedOtherPage(
+                  scoreList: scoreList,
+                  selectedValue: widget.selectedValue,
                 ),
-              ));
+    );
   }
 
   //各類語ごとにポジティブ値を計測する
@@ -138,7 +65,7 @@ class _GetValuePageState extends State<GetValuePage> {
         "https://language.googleapis.com/v1/documents:analyzeSentiment?key=$apiKey";
     Uri uri = Uri.parse(urlString);
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 30; i++) {
       String text = resultWords[i];
 
       Map<String, String> headers = {"content-type": "application/json"};
@@ -166,14 +93,5 @@ class _GetValuePageState extends State<GetValuePage> {
     maxScoreWord = scoreList[scoreList.length - 1].synonym;
 
     setState(() {});
-  }
-
-  toNextPage(BuildContext context) {
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => HomeScreen(),
-      ),
-    );
   }
 }
